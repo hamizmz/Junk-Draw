f2147f64b33f8601b652f413c12785d2532ebc2b
hamizmz <root@graham-robertson.ca>
2016-03-02 14:31:16 -0500

    ╰(✿˙ᗜ˙)੭━☆ﾟ.*･｡ﾟ

---
+++ haxe/src/__files__.cpp
@@ -0,0 +1,20 @@
+#include <hxcpp.h>
+
+namespace hx {
+const char *__hxcpp_all_files[] = {
+#ifdef HXCPP_DEBUGGER
+	"HelloWorld.hx",
+	"Sys.hx",
+	"cpp/Lib.hx",
+	"haxe/io/Eof.hx",
+#endif
+ 0 };
+const char *__hxcpp_class_path[] = {
+#ifdef HXCPP_DEBUGGER
+	"./",
+	"/usr/share/haxe/std/cpp/_std/",
+	"/usr/share/haxe/std/",
+	"/usr/share/haxe/std/",
+#endif
+ 0 };
+} // namespace hx

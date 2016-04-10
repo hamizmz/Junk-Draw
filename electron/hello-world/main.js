9bd25b08b081f464de13e801794b75a8e85a096e
hamizmz <root@graham-robertson.ca>
2016-04-10 16:20:23 -0400

    SOCKETS BABYgit add .

--- electron/hello-world/main.js
+++ electron/hello-world/main.js
@@ -3,6 +3,9 @@
 const electron = require('electron');
 const app = electron.app;
 
+const server = require('http').Server(require('express')());
+const io = require('socket.io')(server);
+
 var _mainWindow = null;
 
 app.on('window-all-closed', function() {
@@ -15,6 +18,7 @@ app.on('ready', function() {
 	_mainWindow = new electron.BrowserWindow({
 		"width": 600,
 		"height": 800,
+		
 		"directWrite": false,
 		"textAreasAreResizable": false,
 		"darkTheme": true,
@@ -31,8 +35,26 @@ app.on('ready', function() {
 	_mainWindow.setMenuBarVisibility(false);
 	_mainWindow.loadURL('file://' + __dirname + '/index.html');
 	
-	// CLEAN-UP
+	// CLEAN-UP Window
 	_mainWindow.on('closed', function() {
 		_mainWindow = null;
+	});
+	
+	
+	// SERVER JUNK
+	io.on('connection', function(socket) {
+		// Template for socket events:
+		socket.on('Message', function(msg) {
+			console.log(msg);
+		});
+		
+		// Do something when socket disconnects?
+		socket.on('disconnect', function() {
+			console.log('User disconnected.');
+		});
+	});
+	
+	server.listen(8989, function() {
+		console.log("Listening on port 8989...");
 	});
 });

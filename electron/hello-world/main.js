f3ebddd5d1ac40860450d21822f86f654f35cdec
hamizmz <root@graham-robertson.ca>
2016-04-13 20:02:46 -0400

    👽 👽 👽  This code is out of this worldclear 👽 👽 👽

--- electron/hello-world/main.js
+++ electron/hello-world/main.js
@@ -1,49 +1,28 @@
 'use strict';
 
+/*
+	REQUIREMENTS
+*/
 const electron = require('electron');
 const app = electron.app;
 
 const server = require('http').Server(require('express')());
 const io = require('socket.io')(server);
 
+const zmq = require('zmq');
 
-
+/*
+	PROPERTIES
+*/
+const WEB_PORT = 8989;
+const ZMQ_PORT = 9898;
 var _window = null;
 
-app.on('window-all-closed', function() {
-	// On OS X, we don't quit the program when all windows are closed (usually)
-	if (process.platform != 'darwin')
-		app.quit();
-});
 
-app.on('ready', function() {
-	_window = new electron.BrowserWindow({
-		"width": 600,
-		"height": 800,
-		"backgroundColor": "#404552",
-		
-		"directWrite": false,
-		"textAreasAreResizable": false,
-		"darkTheme": true,
-		
-		"webPreferences": {
-			"nodeIntegration": false,
-			"java": false,
-			"plugins": false,
-			"webgl": false,
-			"webaudio": false,
-			"defaultEncoding": "UTF-8",
-		},
-	});
-	_window.setMenuBarVisibility(false);
-	_window.loadURL('file://' + __dirname + '/index.html');
-	
-	// CLEAN-UP Window
-	_window.on('closed', function() {
-		_window = null;
-	});
-	
-	
+/*
+	WEB SOCKET JUNK
+*/
+function setup_websocket() {
 	// SERVER JUNK
 	io.on('connection', function(socket) {
 		// Template for socket events:
@@ -57,7 +36,61 @@ app.on('ready', function() {
 		});
 	});
 	
-	server.listen(8989, function() {
-		console.log("Listening on port 8989...");
+	server.listen(WEB_PORT);
+};
+
+
+/*
+	ZMQ SOCKET PAIR
+*/
+function setup_zmq() {
+	/* We are ignoring this for now :) */
+};
+
+
+/*
+	WINDOW JUNK
+*/
+function setup_window() {
+	app.on('window-all-closed', function() {
+		// On OS X, we don't quit the program when all windows are closed (usually)
+		if (process.platform != 'darwin')
+			app.quit();
 	});
-});
+
+	app.on('ready', function() {
+		_window = new electron.BrowserWindow({
+			"width": 600,
+			"height": 800,
+			"backgroundColor": "#404552",
+			
+			"directWrite": false,
+			"textAreasAreResizable": false,
+			"darkTheme": true,
+			
+			"webPreferences": {
+				"nodeIntegration": false,
+				"java": false,
+				"plugins": false,
+				"webgl": false,
+				"webaudio": false,
+				"defaultEncoding": "UTF-8",
+			},
+		});
+		_window.setMenuBarVisibility(false);
+		_window.loadURL('file://' + __dirname + '/index.html');
+		
+		// CLEAN-UP Window
+		_window.on('closed', function() {
+			_window = null;
+		});
+	});
+};
+
+
+/*
+	CALL OUR SETUPS!
+*/
+setup_window();
+setup_websocket();
+setup_zmq(); // Does nothing at the moment

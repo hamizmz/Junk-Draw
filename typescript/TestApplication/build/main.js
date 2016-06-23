913c1512b09b793deb62d0728f325fd56723a0fd
hamizmz <root@graham-robertson.ca>
2016-06-23 05:33:20 -0400

    Just more junk

---
+++ typescript/TestApplication/build/main.js
@@ -0,0 +1,5 @@
+var log = require('terminal-kit').terminal;
+log.white('\n****************************************\n');
+log.white("* ").yellow("TestApplication").white('\n');
+log.white("* ").red('Generated: ').white((new Date()).toString()).white("\n");
+log.white('****************************************\n\n');
